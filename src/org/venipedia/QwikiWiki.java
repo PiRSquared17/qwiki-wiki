02b04079ed82a6966c6913c94cbca7a119a57539
Ryan Muller <ryan.a.muller@gmail.com>
2012-02-12 17:17:31 -0500

    Redid the base UI and started reworking the login screens.

--- src/org/venipedia/QwikiWiki.java
+++ src/org/venipedia/QwikiWiki.java
@@ -1,31 +1,39 @@
 package org.venipedia;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JDesktopPane;
 import java.awt.BorderLayout;
-import java.awt.Toolkit;
-import javax.swing.JList;
-import javax.swing.AbstractListModel;
 import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPopupMenu;
 import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JProgressBar;
+
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JProgressBar;
 import javax.swing.JSeparator;
+
+import org.venipedia.credentials.BluehostCredentials;
+import org.venipedia.credentials.VenipediaCredentials;
+import org.venipedia.ui.login.BluehostLoginFrame;
+import org.venipedia.ui.login.VenipediaLoginFrame;
 
 public class QwikiWiki {
 
 	private JFrame frmQwikiwiki;
+	private JProgressBar progressBar;
+	private JDesktopPane desktopPane;
+
+	private VenipediaCredentials vCred;
+	private BluehostCredentials bCred;
 
 	/**
 	 * Launch the application.
@@ -55,12 +63,13 @@ public class QwikiWiki {
 	 */
 	private void initialize() {
 		frmQwikiwiki = new JFrame();
+		frmQwikiwiki.setExtendedState(Frame.MAXIMIZED_BOTH);
 		frmQwikiwiki.setTitle("QwikiWiki");
 		frmQwikiwiki.setIconImage(Toolkit.getDefaultToolkit().getImage(QwikiWiki.class.getResource("/venicon.png")));
 		frmQwikiwiki.setBounds(100, 100, 627, 421);
 		frmQwikiwiki.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane = new JDesktopPane();
 		desktopPane.setBackground(new Color(102, 0, 0));
 		frmQwikiwiki.getContentPane().add(desktopPane, BorderLayout.CENTER);
 		
@@ -74,19 +83,44 @@ public class QwikiWiki {
 			}
 		});
 		
+		JMenu mnConnect = new JMenu("Connect");
+		mnConnect.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/direction.png")));
+		popupMenu.add(mnConnect);
+		
+		JMenuItem mntmLogInTo = new JMenuItem("Log in to Venipedia");
+		mntmLogInTo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				venipediaLogIn();
+			}
+		});
+		mntmLogInTo.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/monitor-image.png")));
+		mnConnect.add(mntmLogInTo);
+		
+		JMenuItem mntmLogInTo_1 = new JMenuItem("Log in to Bluehost");
+		mntmLogInTo_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				bluehostLogIn();
+			}
+		});
+		mntmLogInTo_1.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/monitor-window.png")));
+		mnConnect.add(mntmLogInTo_1);
+		
 		JMenu mnUpload = new JMenu("Upload");
 		mnUpload.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/upload-cloud.png")));
 		popupMenu.add(mnUpload);
 		
 		JMenuItem mntmTablecsv = new JMenuItem("Table (CSV)");
+		mntmTablecsv.setEnabled(false);
 		mntmTablecsv.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/table-excel.png")));
 		mnUpload.add(mntmTablecsv);
 		
 		JMenuItem mntmGoogleEarthkml = new JMenuItem("Google Earth (KML)");
+		mntmGoogleEarthkml.setEnabled(false);
 		mntmGoogleEarthkml.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/globe-green.png")));
 		mnUpload.add(mntmGoogleEarthkml);
 		
 		JMenuItem mntmFolderOfPictures = new JMenuItem("Folder of pictures");
+		mntmFolderOfPictures.setEnabled(false);
 		mntmFolderOfPictures.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/photo-album.png")));
 		mnUpload.add(mntmFolderOfPictures);
 		
@@ -95,10 +129,12 @@ public class QwikiWiki {
 		popupMenu.add(mnCreate);
 		
 		JMenuItem mntmPage = new JMenuItem("Page");
+		mntmPage.setEnabled(false);
 		mntmPage.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/blog-blue.png")));
 		mnCreate.add(mntmPage);
 		
 		JMenuItem mntmPagesFromDatabase = new JMenuItem("Pages from database table");
+		mntmPagesFromDatabase.setEnabled(false);
 		mntmPagesFromDatabase.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/blogs-stack.png")));
 		mnCreate.add(mntmPagesFromDatabase);
 		
@@ -107,10 +143,12 @@ public class QwikiWiki {
 		popupMenu.add(mnOrganize);
 		
 		JMenuItem mntmCategorizePages = new JMenuItem("Categorize pages");
+		mntmCategorizePages.setEnabled(false);
 		mntmCategorizePages.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/drawer-open.png")));
 		mnOrganize.add(mntmCategorizePages);
 		
 		JMenuItem mntmDeletePages = new JMenuItem("Delete pages");
+		mntmDeletePages.setEnabled(false);
 		mntmDeletePages.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/bomb.png")));
 		mnOrganize.add(mntmDeletePages);
 		
@@ -119,10 +157,12 @@ public class QwikiWiki {
 		popupMenu.add(mnView);
 		
 		JMenuItem mntmVenipedia = new JMenuItem("Venipedia");
+		mntmVenipedia.setEnabled(false);
 		mntmVenipedia.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/application-browser.png")));
 		mnView.add(mntmVenipedia);
 		
 		JMenuItem mntmListPages = new JMenuItem("List pages");
+		mntmListPages.setEnabled(false);
 		mntmListPages.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/edit-list.png")));
 		mnView.add(mntmListPages);
 		
@@ -131,6 +171,7 @@ public class QwikiWiki {
 		popupMenu.add(mnHelp);
 		
 		JMenuItem mntmReportBug = new JMenuItem("Report bug");
+		mntmReportBug.setEnabled(false);
 		mntmReportBug.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/bug.png")));
 		mnHelp.add(mntmReportBug);
 		
@@ -143,7 +184,8 @@ public class QwikiWiki {
 		mntmExit.setIcon(new ImageIcon(QwikiWiki.class.getResource("/icons/door-open-out.png")));
 		popupMenu.add(mntmExit);
 		
-		JProgressBar progressBar = new JProgressBar();
+		progressBar = new JProgressBar();
+		progressBar.setForeground(new Color(128, 128, 128));
 		progressBar.setBorderPainted(false);
 		progressBar.setBorder(null);
 		progressBar.setBackground(new Color(102, 0, 0));
@@ -152,6 +194,24 @@ public class QwikiWiki {
 		frmQwikiwiki.getContentPane().add(progressBar, BorderLayout.SOUTH);
 	}
 	
+
+	protected void bluehostLogIn() {
+		showWindow(new BluehostLoginFrame(this));
+	}
+
+	public void setStatus(String s){
+		getProgressBar().setString(s);
+	}
+	
+	protected void venipediaLogIn() {
+		showWindow(new VenipediaLoginFrame(this));
+	}
+
+	private void showWindow(JInternalFrame frame) {
+		getDesktopPane().add(frame);
+		frame.setVisible(true);
+	}
+
 	protected void exit() {
 		System.exit(0);
 	}
@@ -172,5 +232,27 @@ public class QwikiWiki {
 				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
 		});
+	}
+	protected JProgressBar getProgressBar() {
+		return progressBar;
+	}
+	protected JDesktopPane getDesktopPane() {
+		return desktopPane;
+	}
+
+	public VenipediaCredentials getvCred() {
+		return vCred;
+	}
+
+	public void setvCred(VenipediaCredentials vCred) {
+		this.vCred = vCred;
+	}
+
+	public BluehostCredentials getbCred() {
+		return bCred;
+	}
+
+	public void setbCred(BluehostCredentials bCred) {
+		this.bCred = bCred;
 	}
 }
---
+++ src/org/venipedia/ui/login/BluehostLoginFrame.java
@@ -0,0 +1,137 @@
+package org.venipedia.ui.login;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.venipedia.QwikiWiki;
+import org.venipedia.credentials.BluehostCredentials;
+
+public class BluehostLoginFrame extends JInternalFrame {
+	private JTextField txtUsername;
+	private JPasswordField passwordField;
+	
+	private QwikiWiki parent;
+
+	/**
+	 * Create the frame.
+	 * @param qwikiWiki 
+	 */
+	public BluehostLoginFrame(QwikiWiki qwikiWiki) {
+		parent = qwikiWiki;
+		setTitle("Log in to Bluehost");
+		setFrameIcon(new ImageIcon(BluehostLoginFrame.class.getResource("/icons/monitor.png")));
+		setBounds(100, 100, 450, 300);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblUserName = new JLabel("User name:");
+		GridBagConstraints gbc_lblUserName = new GridBagConstraints();
+		gbc_lblUserName.anchor = GridBagConstraints.EAST;
+		gbc_lblUserName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUserName.gridx = 0;
+		gbc_lblUserName.gridy = 0;
+		getContentPane().add(lblUserName, gbc_lblUserName);
+		
+		txtUsername = new JTextField();
+		txtUsername.setToolTipText("Type your Venipedia username");
+		GridBagConstraints gbc_txtUsername = new GridBagConstraints();
+		gbc_txtUsername.gridwidth = 2;
+		gbc_txtUsername.insets = new Insets(0, 0, 5, 0);
+		gbc_txtUsername.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtUsername.gridx = 1;
+		gbc_txtUsername.gridy = 0;
+		getContentPane().add(txtUsername, gbc_txtUsername);
+		txtUsername.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.anchor = GridBagConstraints.EAST;
+		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPassword.gridx = 0;
+		gbc_lblPassword.gridy = 1;
+		getContentPane().add(lblPassword, gbc_lblPassword);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.gridwidth = 2;
+		gbc_passwordField.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 1;
+		getContentPane().add(passwordField, gbc_passwordField);
+		
+		JButton btnLogIn = new JButton("Log in");
+		btnLogIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logIn();
+			}
+		});
+		btnLogIn.setIcon(new ImageIcon(BluehostLoginFrame.class.getResource("/icons/thumb-up.png")));
+		GridBagConstraints gbc_btnLogIn = new GridBagConstraints();
+		gbc_btnLogIn.gridwidth = 2;
+		gbc_btnLogIn.insets = new Insets(0, 0, 0, 5);
+		gbc_btnLogIn.gridx = 0;
+		gbc_btnLogIn.gridy = 2;
+		getContentPane().add(btnLogIn, gbc_btnLogIn);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cancel();
+			}
+		});
+		btnCancel.setIcon(new ImageIcon(BluehostLoginFrame.class.getResource("/icons/cross.png")));
+		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+		gbc_btnCancel.gridx = 2;
+		gbc_btnCancel.gridy = 2;
+		getContentPane().add(btnCancel, gbc_btnCancel);
+		
+		populateFromCreds();
+		
+		pack();
+	}
+	
+	private void populateFromCreds(){
+		if(parent.getbCred()==null) return;
+		txtUsername.setText(parent.getbCred().getUsername());
+		passwordField.setText(parent.getbCred().getPassword());
+	}
+
+	protected void cancel() {
+		closeOut();
+	}
+
+	protected void logIn() {
+		String username = getTxtUsername().getText();
+		String password = new String(getPasswordField().getPassword());
+		BluehostCredentials bCreds =new BluehostCredentials(username, password); 
+		parent.setbCred(bCreds);
+		parent.setStatus("Logged in to Bluehost as "+username+".");
+		closeOut();
+	}
+	
+	protected void closeOut(){
+		dispose();
+	}
+
+	protected JTextField getTxtUsername() {
+		return txtUsername;
+	}
+	public JPasswordField getPasswordField() {
+		return passwordField;
+	}
+}
